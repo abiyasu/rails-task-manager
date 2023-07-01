f4eb0c7bb9c342774fb3c66250ea171921a90e2f
Abiyasu Zhang <abiyasuzhang@gmail.com>
2023-07-01 16:56:48 +0900

    homework

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'list', to: 'tasks#list', as: 'list'
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
